16f3d2e358c1503ed06e3529f6c54777ad78bcd4
yahya benzha <yahya_benzha@um5.ac.ma>
2022-03-24 00:48:02 +0100

    read from db

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,33 @@
-import {
-  initializeApp,
-  applicationDefault,
-  getApps,
-  getApp,
-} from 'firebase-admin/app'
-import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore'
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAO6QOxsKSYQR3Wi3pnAi77xoa6qikhhtU',
-  authDomain: 'ecommerce-37950.firebaseapp.com',
-  projectId: 'ecommerce-37950',
-  storageBucket: 'ecommerce-37950.appspot.com',
-  messagingSenderId: '783618676442',
-  appId: '1:783618676442:web:afc5929d3cf467e555f7e3',
+// import {
+//   initializeApp,
+//   applicationDefault,
+//   getApps,
+//   getApp,
+// } from 'firebase-admin/app'
+// import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore'
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyAO6QOxsKSYQR3Wi3pnAi77xoa6qikhhtU',
+//   authDomain: 'ecommerce-37950.firebaseapp.com',
+//   projectId: 'ecommerce-37950',
+//   storageBucket: 'ecommerce-37950.appspot.com',
+//   messagingSenderId: '783618676442',
+//   appId: '1:783618676442:web:afc5929d3cf467e555f7e3',
+// }
+
+// const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+// export const db = getFirestore(app)
+
+import { getFirestore, FieldValue } from 'firebase-admin/firestore'
+import { initializeApp, cert, getApp, getApps } from 'firebase-admin/app'
+
+const serviceAccount = require('../../permissions.json')
+
+if (!getApps().length) {
+  const app = initializeApp({
+    credential: cert(serviceAccount),
+  })
+} else {
+  const app = getApp()
 }
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
-
-export const db = getFirestore(app)
+export const db = getFirestore()
