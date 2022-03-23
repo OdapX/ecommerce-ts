150803e001fafae5f1d2701aa8b3375c015f8aa1
yahya benzha <yahya_benzha@um5.ac.ma>
2022-03-23 18:20:32 +0100

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,13 +7,13 @@ import EmailProvider from 'next-auth/providers/email'
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    
+
     GoogleProvider({
-      clientId : process.env.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
-     ],
-     callbacks: {
+  ],
+  callbacks: {
     session({ session, token, user }) {
       return session || null
     },
