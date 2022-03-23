a779ab09912e91e78e7562fd300b86e4162128e0
yahya benzha <yahya_benzha@um5.ac.ma>
2022-03-23 18:15:18 +0100

    type command import

--- pages/api/create-session.ts
+++ pages/api/create-session.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import Command from "../../typings"
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+import { Command } from '../../typings'
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || '')
 type Data = {
   id: string
 }
@@ -10,36 +11,34 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-   
-    const {commands,email} = JSON.parse(req.body)
-    
-    const line_items = commands.map((command : Command) =>({
-        description: command.product.description,
-        price_data: {
-          currency: 'eur',
-          product_data: {
-            name: command.product.title,
-            images: [command.product.image]
-          },
-          unit_amount: command.product.price *100,
-        },
-        quantity: command.quantity,
-      }))
-      const session = await stripe.checkout.sessions.create({
-      line_items,
-      shipping_rates:['shr_1KfDwIB4LrBkOCLdo2xZwPgT'],
-     
-      mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_HOST}/success`,
-       cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout`,
-       metadata:{
-           email,
-           images : JSON.stringify(commands.map((command : Command) => command.product.image))
-       }
-   });
-    
-   
-    
-    res.status(200).json({id : session.id})
+  const { commands, email } = JSON.parse(req.body)
 
+  const line_items = commands.map((command: Command) => ({
+    description: command.product.description,
+    price_data: {
+      currency: 'eur',
+      product_data: {
+        name: command.product.title,
+        images: [command.product.image],
+      },
+      unit_amount: command.product.price * 100,
+    },
+    quantity: command.quantity,
+  }))
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    shipping_rates: ['shr_1KfDwIB4LrBkOCLdo2xZwPgT'],
+
+    mode: 'payment',
+    success_url: `${process.env.NEXT_PUBLIC_HOST}/success`,
+    cancel_url: `${process.env.NEXT_PUBLIC_HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(
+        commands.map((command: Command) => command.product.image)
+      ),
+    },
+  })
+
+  res.status(200).json({ id: session.id })
 }
